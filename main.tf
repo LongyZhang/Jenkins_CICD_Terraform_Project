bc32b78d1dcde32eaf1b3971b54108a4feb2140e
longy zhang <longhanzhang999@gmail.com>
2025-07-29 21:50:39 +1000

    debug the NAT for whole day, trying to install the softether VPN on ec2, so it can be my vpn server to access the resource from private subnet

--- main.tf
+++ main.tf
@@ -46,6 +46,48 @@ resource "aws_subnet" "public" {
   tags = { Name = "public-subnet" }
 }
 
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "us-east-1a"  # Adjust as needed
+  tags = { Name = "private-subnet" }
+  
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "public_nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+  depends_on    = [aws_internet_gateway.igw]
+  tags =  {Name = "public-nat-gateway"}  
+}
+
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+
+resource "aws_route" "private_rt_nat" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.public_nat.id
+}
+
+
+resource "aws_route_table_association" "assoc_private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+  
+}
+
+
+
+
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.main.id
 }
@@ -80,6 +122,18 @@ resource "aws_security_group" "ssh_sg" {
   }
 }
 
+resource "aws_security_group" "rdp" {
+  name= "allow_rdp"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.my_ip.body)}/32"]
+  }
+}
+
+
 resource "aws_security_group" "vpc_sg" {
   name   = "allow access from subnet"
   vpc_id = aws_vpc.main.id
@@ -108,21 +162,28 @@ resource "aws_key_pair" "my_key" {
 # List of node names
 locals {
   instance_names = [
-    "gitlab-node",
-    "Jenkins-node",
+    "GITLAB-node",
+    "JENKINS-node",
     "DOCKER-node"
+    
   ]
 }
 
+locals {
+  public_instance_names = [
+    "VPN-node"
+  ]
+}
 
 
 resource "aws_instance" "nodes" {
   count         = length(local.instance_names)
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "m4.large"
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.private.id
   key_name      = aws_key_pair.my_key.key_name
   vpc_security_group_ids = [aws_security_group.ssh_sg.id,aws_security_group.vpc_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
 
 
   user_data = file("user_data/user_data.sh")
@@ -134,3 +195,63 @@ resource "aws_instance" "nodes" {
 
 
 
+data "aws_ami" "windows" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2022-English-Full-Base-*"]
+  }
+
+  filter { 
+    name   = "platform"
+    values = ["windows"]
+  }
+
+  owners = ["801119661308"] # Amazon's official Windows AMIs
+}
+
+
+resource "aws_instance" "public_nodes" {
+  count         = length(local.public_instance_names)
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "m4.large"
+  subnet_id     = aws_subnet.public.id
+  key_name      = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id,aws_security_group.vpc_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+
+  # get_password_data = true
+  user_data = file("user_data/user_data.sh")
+
+  tags = {
+    Name = local.public_instance_names[count.index]
+  }
+}
+
+
+
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ssm-profile"
+  role = aws_iam_role.ssm_role.name
+}
--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -8,3 +8,8 @@ echo "Ansible version:" >> /home/ec2-user/user_data.log
 ansible --version >> /home/ec2-user/user_data.log
 sudo -u ec2-user pip3 install --user ansible
 
+#!/bin/bash
+cd /tmp
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
